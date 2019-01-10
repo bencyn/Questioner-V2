731a51b8aed690f612f5cf7949a15d4ef74f4ff6
bencyn <bensonnjung39@gmail.com>
2019-01-10 21:44:40 +0300

    [ch #163113288] add rsvp endpoint tests

--- tests/v1/test_meetups.py
+++ tests/v1/test_meetups.py
@@ -37,49 +37,48 @@ class TestUser(BaseTest):
                 "images":"https://bencyn.github.io/Questioner/UI/images/456470.jpeg,https://bencyn.github.io/Questioner/UI/images/475058220.jpeg"
             
             }]
-
+        self.rsvp={"status":"yes"}
+        self.get_url = 'api/v1/meetups/2'
+        self.post_url ='api/v1/meetups/'
 
     def test_user_can_create_meeetup_record(self):
         with self.client:
             ''' test user can create a meetup record'''
             # url = api/v1/meetups/
             response= self.client.post('api/v1/meetups/', data = json.dumps(self.meetups[0]), content_type="application/json")
-            # result = json.loads(response.data.decode('UTF-8'))
+          
             self.assertEqual(response.status_code, 201)
    
     def test_user_can_get_meetup_records(self):
-            ''' test user can get all meetup records '''
-
-            post_url = 'api/v1/meetups/upcoming/'
-            response = self.client.get(post_url,content_type = "application/json")
-            self.assertEqual(response.status_code, 200)
+        ''' test user can get all meetup records '''
+        
+        response = self.client.get(self.get_url,content_type = "application/json")
+        self.assertEqual(response.status_code, 200)
 
     def test_user_can_get_specific_meetup_record(self):
         ''' test whether user can get a specific record using it's meetup id '''
 
-        get_url = 'api/v1/meetups/2'
-        # post_url ='api/v1/meetups/'
-
-        self.client.post('api/v1/meetups/', data = json.dumps(self.meetups[0]), content_type="application/json")
+        self.client.post(self.post_url, data = json.dumps(self.meetups[0]), content_type="application/json")
         self.client.post('api/v1/meetups/', data = json.dumps(self.meetups[1]), content_type="application/json")
 
-        response = self.client.get(get_url, content_type="application/json")
+        response = self.client.get(self.get_url, content_type="application/json")
         self.assertEqual(response.status_code,200)
 
-    # def test_get_meetup_specific_record(self):
-    #         '''test user can get a specific record using meetup'''
+    def test_rsvp_meetup(self):
+        ''' tests whether user can respond to a meeting invitation '''
 
-    #         url = 'api/v1/meetups/2'
+        rsvp_url ='api/v1/meetups/1/rsvps'
 
-    #         self.client.post("api/v1/meetups", json.dumps(self.meetup1), content_type = "application/json")
-    #         self.client.post("api/v1/meetups", json.dumps(self.meetup2),  content_type = "application/json")
+        # post two meetup records
+        self.client.post('api/v1/meetups/', data = json.dumps(self.meetups[0]), content_type="application/json")
+        self.client.post('api/v1/meetups/', data = json.dumps(self.meetups[1]), content_type="application/json")
+        
+        response = self.client.post(rsvp_url,data=json.dumps(self.rsvp) ,content_type="application/json")
+        result = json.loads(response.data.decode('UTF-8'))
+        self.assertEqual(response.status_code,201)
+        self.assertEqual(result["data"]['status'],'yes')
+        # self.assertEqual(response)
 
-    #         response = self.client.get(url, content_type = "application/json")
-    #         self.assertEqual(response.status_code, 200)
-
-    #         result = json.loads(response.data.decode('utf-8'))
-    #         self.assertEqual(result['status'], 200)
-          
 
 if __name__ == "__main__":
     unittest.main()
