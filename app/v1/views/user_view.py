854cf1414e8683e58736a1e6ddc6ca6b7a1f6844
bencyn <bensonnjung39@gmail.com>
2019-01-11 02:11:17 +0300

    [ft #163131718] add get all and register endpoint

--- app/v1/views/user_view.py
+++ app/v1/views/user_view.py
@@ -3,26 +3,22 @@ from datetime import datetime
 # from ..models import user_model
 from app.v1.models import user_model
 
-user = Blueprint('meetup', __name__, url_prefix='/api/v1/users')
+user = Blueprint('user', __name__, url_prefix='/api/v1/users')
 user_object = user_model.User()
 
 
 @user.route("/all", methods=['GET'])
 def getUsers():
-    ''' fetch all user recorce'''
+    ''' fetch all users'''
     return jsonify(user_object.get_users()),200
 
-
-# user signup and get users
-@user.route("/create", methods=['POST'])
+@user.route('/', methods = ['POST'])
 def register():
     """user signup"""
     data = request.get_json()
 
     if not data:
-        return jsonify({"Message": 'Cannot send empty data'})
-    if not all(field in data for field in ['username', 'email','password']):
-        return jsonify({"Message": "All fields are required"})
+        return jsonify({"Message": 'Cannot send empty data'}),409
     
     firstname = request.get_json()['firstname']
     lastname = request.get_json()['lastname']
@@ -32,34 +28,24 @@ def register():
     password = request.get_json()['password']
     username = request.get_json()['username']
     isAdmin = request.get_json()['isAdmin']
- 
-    if not firstname:
-        return validate_input("firstname")
-    if not lastname:
-        return validate_input("lastname")
-    if not othername:
-        return validate_input("othername")
-    if not email:
-        return validate_input("email")
-    if not phoneNUmber:
-        return validate_input("phoneNUmber")
-    if not password:
-        return validate_input("password")
+
     if not username:
         return validate_input("username")
-    
-    
-    if any(i['username'] == username for i in user_object.users):
-        return jsonify({'msg': 'username already exists'}), 409
-    if any(i['email'] == email for i in user_object.users):
-        return jsonify({'msg': 'email already exists'}), 409
+    if not email:
+        return validate_input("email")
+    if not password:
+        return validate_input("password")
     else:
         # add create user
-        user_object.create_user(firstname,lastname,othername,email,password,phoneNUmber,username,isAdmin)
-        return jsonify({'user': user}), 201
+        user = user_object.create_user(firstname,lastname,othername,email,password,phoneNUmber,username,isAdmin)
+        return jsonify({
+            "status": 201,
+            "data":user
+        }), 201
 
 def validate_input(field):
      return make_response(jsonify({
             "status": 400,
             "message": "{} cannot be empty".format(field)
         })), 400
+
