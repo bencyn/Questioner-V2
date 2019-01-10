8922bccb7905740286a1947f4e300cd946202d7c
bencyn <bensonnjung39@gmail.com>
2019-01-11 02:29:04 +0300

    [ft #163131718] add unit tests for get users and get by if user

--- tests/v1/test_users.py
+++ tests/v1/test_users.py
@@ -10,7 +10,8 @@ class TestUser(BaseTest):
         app.config.from_object(instance.config.TestingConfig)
         self.client = app.test_client()
 
-        self.user = {
+        self.users =[
+            {
                 "firstname" :"benson",
                 "lastname": "kamaa",
                 "othername" :"wamolito",
@@ -18,14 +19,26 @@ class TestUser(BaseTest):
                 "password":"ben74285",
                 "phoneNUmber":"0790561841",
                 "username" :"njeru",
-                "isAdmin" :"0"
+                "isAdmin" :"0",
+            },
+            {
+                "firstname" :"benson",
+                "lastname": "kamaa",
+                "othername" :"wamolito",
+                "email" :"bensonnjung39@gmail.com",
+                "password":"ben74285",
+                "phoneNUmber":"0790561841",
+                "username" :"njeru",
+                "isAdmin" :"0",
             }
+        ]
         self.post_url = 'api/v1/users/'
+        self.get_url = 'api/v1/users/all'
 
     def test_if_user_can_create_account(self):
         with self.client:
             ''' test user can create an account successfully'''
-            response= self.client.post(self.post_url, data = json.dumps(self.user), content_type="application/json")
+            response= self.client.post(self.post_url, data = json.dumps(self.users[0]), content_type="application/json")
             self.assertEqual(response.status_code, 201)
 
     def test_empty_post_data(self):
@@ -46,3 +59,15 @@ class TestUser(BaseTest):
             }
         response= self.client.post(self.post_url, data = json.dumps(self.user3), content_type="application/json")
         self.assertEqual(response.status_code,400)
+
+    def test_get_all_users(self):
+        response = self.client.get(self.get_url,content_type = "application/json")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_user_by_id(self):
+        self.get_by_id_url = 'api/v1/users/2'
+        self.client.post(self.post_url, data = json.dumps(self.users[0]), content_type="application/json")
+        self.client.post('api/v1/meetups/', data = json.dumps(self.users[1]), content_type="application/json")
+
+        response = self.client.get(self.get_by_id_url, content_type="application/json")
+        self.assertEqual(response.status_code,200)
