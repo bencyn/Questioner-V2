49d1e0a9192e8097feda8030aae48134780e5343
bencyn <bensonnjung39@gmail.com>
2019-01-10 20:48:53 +0300

    [ch #163113288] add tests to post and get endpoints

--- tests/v1/test_meetups.py
+++ tests/v1/test_meetups.py
@@ -1,8 +1,8 @@
 import json ,unittest,instance,datetime
 from .base_tests import BaseTest
-from app.v1.models.meetup_model import Meetup
-from app.v1.views import meetup_view
-from app.v1.models import meetup_model
+# from ..models import meetup_model
+# from app.v1.views import meetup_view
+# from app.v1.models import meetup_model 
 from app import create_app
 
 app = create_app("testing")
@@ -20,70 +20,51 @@ class TestUser(BaseTest):
             "happeningOn": "Monday 12 2018",
             "tags": ["Pentests", "Bruteforce"]
         }
-
-        self.meetup1 = {
-                "id":1,
-                "createdOn":"Wed,10 Jan 2019",
-                "location":"Nakuru",
-                "images":["img1.png","img2.png"],
-                "topic":"empowering local talent",
-                "happeningOn":"Friday, 20 Feb 2019",
-                "Tags":['dunda','gospel','reggae']
-            }
-        self.meetup2 = {
-                "id":2,
-                "createdOn":"Fri,10 Jan 2019",
-                "location":"Nyahururu",
-                "images":["img1.png","img2.png"],
-                "topic":"Antivals Hackathon",
-                "happeningOn":"Friday, 28 Feb 2019",
-                "Tags":['startup','innovation','passion']
-        }
-        self.meetups = [{
-                "id":1,
-                "createdOn":"Wed,10 Jan 2019",
-                "location":"Nakuru",
-                "images":["img1.png","img2.png"],
-                "topic":"empowering local talent",
-                "happeningOn":"Friday, 20 Feb 2019",
-                "Tags":['dunda','gospel','reggae']
+        self.meetups = [
+            {
+                "location":"nairobi",
+                "topic":"Hackathon For The Brave",
+                "happeningOn":"2019-20-23",
+                "tags":"UI,UX",
+                "images":"https://bencyn.github.io/Questioner/UI/images/456471610.jpeg,https://bencyn.github.io/Questioner/UI/images/475058220.jpeg"
+            
             },
             {
-                "id":2,
-                "createdOn":"Fri,10 Jan 2019",
-                "location":"Nyahururu",
-                "images":["img1.png","img2.png"],
-                "topic":"Antivals Hackathon",
-                "happeningOn":"Friday, 28 Feb 2019",
-                "Tags":['startup','innovation','passion']
-            }
-        ]
+                "location":"nyahururu",
+                "topic":"Ethical Hacking Hackathon",
+                "happeningOn":"Monday 12 2018",
+                "tags": ["Pentests", "Bruteforce"],
+                "images":"https://bencyn.github.io/Questioner/UI/images/456470.jpeg,https://bencyn.github.io/Questioner/UI/images/475058220.jpeg"
+            
+            }]
 
-    # def test_meeetup_create_record(self):
-    #         ''' test user can create a meetup record'''
-    #         url = 'api/v1/meetups'
-    #         response= self.client.post(url, json.dumps(self.meetup), content_type="application/json")
-    #         result = json.loads(response.data.decode('UTF-8'))
 
-    #         self.assertEqual(response.status_code, 201)
-    #         self.assertEqual(result["status"], 201)
-    #         self.assertEqual(result["data"], [
-    #             {
-    #                 "createdOn":datetime.datetime.now(),
-    #                 "topic": "Python",
-    #                 "location": "Nairobi",
-    #                 "happeningOn": "Thursday",
-    #                 "tags": ["RESTful API", "JSON Data"],
-    #             }
-    #         ])
-
+    def test_user_can_create_meeetup_record(self):
+        with self.client:
+            ''' test user can create a meetup record'''
+            # url = api/v1/meetups/
+            response= self.client.post('api/v1/meetups/', data = json.dumps(self.meetups[0]), content_type="application/json")
+            # result = json.loads(response.data.decode('UTF-8'))
+            self.assertEqual(response.status_code, 201)
+   
     def test_user_can_get_meetup_records(self):
             ''' test user can get all meetup records '''
 
-            url = 'api/v1/meetups/upcoming/'
-            response = self.client.get(url,content_type = "application/json")
+            post_url = 'api/v1/meetups/upcoming/'
+            response = self.client.get(post_url,content_type = "application/json")
             self.assertEqual(response.status_code, 200)
 
+    def test_user_can_get_specific_meetup_record(self):
+        ''' test whether user can get a specific record using it's meetup id '''
+
+        get_url = 'api/v1/meetups/2'
+        # post_url ='api/v1/meetups/'
+
+        self.client.post('api/v1/meetups/', data = json.dumps(self.meetups[0]), content_type="application/json")
+        self.client.post('api/v1/meetups/', data = json.dumps(self.meetups[1]), content_type="application/json")
+
+        response = self.client.get(get_url, content_type="application/json")
+        self.assertEqual(response.status_code,200)
 
     # def test_get_meetup_specific_record(self):
     #         '''test user can get a specific record using meetup'''
@@ -98,15 +79,7 @@ class TestUser(BaseTest):
 
     #         result = json.loads(response.data.decode('utf-8'))
     #         self.assertEqual(result['status'], 200)
-    #         self.assertEqual(result['data'], [{
-    #                                         "id":2,
-    #                                         "createdOn":"Fri,10 Jan 2019",
-    #                                         "location":"Nyahururu",
-    #                                         "images":["img1.png","img2.png"],
-    #                                         "topic":"Antivals Hackathon",
-    #                                         "happeningOn":"Friday, 28 Feb 2019",
-    #                                         "Tags":['startup','innovation','passion']
-    #                                         }])
+          
 
 if __name__ == "__main__":
     unittest.main()
