c97baa79e8c59d16570a935c2c73310db930afd4
bencyn <bensonnjung39@gmail.com>
2019-01-11 02:12:59 +0300

    [ft #163131718] add unit tests for user view endpoints and validations

---
+++ tests/v1/test_users.py
@@ -0,0 +1,48 @@
+import json ,unittest,instance,datetime
+from .base_tests import BaseTest
+from app import create_app
+
+app = create_app("testing")
+
+class TestUser(BaseTest):
+    def setUp(self):
+        """ defining test data"""
+        app.config.from_object(instance.config.TestingConfig)
+        self.client = app.test_client()
+
+        self.user = {
+                "firstname" :"benson",
+                "lastname": "kamaa",
+                "othername" :"wamolito",
+                "email" :"bensonnjung39@gmail.com",
+                "password":"ben74285",
+                "phoneNUmber":"0790561841",
+                "username" :"njeru",
+                "isAdmin" :"0"
+            }
+        self.post_url = 'api/v1/users/'
+
+    def test_if_user_can_create_account(self):
+        with self.client:
+            ''' test user can create an account successfully'''
+            response= self.client.post(self.post_url, data = json.dumps(self.user), content_type="application/json")
+            self.assertEqual(response.status_code, 201)
+
+    def test_empty_post_data(self):
+        self.user2=[]
+        response= self.client.post(self.post_url, data = json.dumps(self.user2), content_type="application/json")
+        self.assertEqual(response.status_code,409)
+
+    def test_required_username_email_and_password(self):
+        self.user3={
+                "firstname" :"benson",
+                "lastname": "kamaa",
+                "othername" :"wamolito",
+                "email" :"",
+                "password":"",
+                "phoneNUmber":"0790561841",
+                "username" :"",
+                "isAdmin" :"0"
+            }
+        response= self.client.post(self.post_url, data = json.dumps(self.user3), content_type="application/json")
+        self.assertEqual(response.status_code,400)
