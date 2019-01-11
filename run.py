811afcae61328025791d0264a3fa9da7c131e92a
Benson Njungé <bensonnjung39@gmail.com>
2019-01-11 16:58:50 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ from app import create_app
 config_name= 'development'
 app = create_app(config_name)
 
+@app.route('/')
+def index():
+    return "Hello there! Welcome to questioner"
+
 if __name__ == "__main__":
     app.run(debug=True)
     
