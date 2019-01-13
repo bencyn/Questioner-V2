a65899d34766ad128b34a6919767a33d338a3eb7
bencyn <bensonnjung39@gmail.com>
2019-01-13 10:03:52 +0300

    [ft #163165404] add missing data validation method

--- app/api/v1/utils/validators.py
+++ app/api/v1/utils/validators.py
@@ -2,12 +2,18 @@ from flask import json, jsonify , make_response
 
 
 class Validators():
-
+    """ global validation methods """
     def __init__(self):
         pass
 
     def validate_input(self,field):
+        """ validates empty input"""
         return make_response(jsonify({
                 "status": 400,
-                "message": "{} cannot be empty".format(field)
+                "error": "{} cannot be empty".format(field)
             })), 400
+    
+    def validate_missing_data(self):
+        """ validates missing json object"""
+        return jsonify({'status': 400,
+                        ' error': "Bad request: attach missing fields"})
