a632ec2beea7dd6f442f6520a5970c5f13462f64
bencyn <bensonnjung39@gmail.com>
2019-01-15 05:13:21 +0300

    [finishes #163165404] add data output tests and error output tests

--- tests/v1/test_users.py
+++ tests/v1/test_users.py
@@ -78,30 +78,51 @@ class TestUser(BaseTest):
         self.assertEqual(response.status_code,200)
     
     def test_if_user_can_login(self):
+        """ tests if user can login successfully with correct credentials """
+
         self.loging_data = {"username":"bencyn","password":"ben742285"}
         self._post_register_request(self.users[0])
         response = self._post_login_request(self.loging_data)
         self.assertEqual(response.status_code,201)
+        # test if data output matches our input
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(result["data"]["username"],self.loging_data["username"])
+        self.assertEqual(result["data"]["password"],self.loging_data["password"])
       
     
     def test_login_username_empty(self):
+        """tests if user can login with an empty username string provided"""
+
         self.username_empty={"username":"","password":"ben742285"}
         self._post_register_request(self.users[0])
         response = self._post_login_request(self.username_empty)
+
+        result = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code,400)
+        self.assertEqual(result["error"],"username cannot be empty")
 
     def test_login_password_empty(self):
+        """tests if user can login with an empty password string provided"""
+
         self.password_empty={"username":"bencyn","password":""}
         self._post_register_request(self.users[0])
         response = self._post_login_request(self.password_empty)
         self.assertEqual(response.status_code,400)
+        result =json.loads(response.data)
+        self.assertEqual(result["error"],"password cannot be empty")
+       
 
     def test_missing_login_data(self):
+        """ tests if user can login without providing any data """
+
         response = self._post_login_request()
         self.assertEqual(response.status_code,400)
+
+        result= json.loads(response.data)
+        self.assertEqual(result["error"],"Bad request: attach missing fields")
     
     def _post_login_request(self,input=""):
-        """  """
+        """ sends a post login request with the input passed as data """
         if input:
             self.data = json.dumps(input)
             response = self.client.post(self.login_url, data = self.data, content_type="application/json")
@@ -111,7 +132,7 @@ class TestUser(BaseTest):
         return response
    
     def _post_register_request(self,input=""):
-        """ """
+        """ send a post regiter request with the input passed as data"""
         if input is True:
             response = self.client.post(self.register_url, data = self.data, content_type="application/json")
         else:
@@ -119,8 +140,5 @@ class TestUser(BaseTest):
 
         return response
        
-    def __get_user_request(self):
-        pass
-        
         
     
