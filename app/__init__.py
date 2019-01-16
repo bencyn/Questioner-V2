3f7f679af460291a51417989370e48b6fa98c37a
bencyn <bensonnjung39@gmail.com>
2019-01-16 23:52:35 +0300

    [ch #163263258] register renamed blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -4,23 +4,25 @@ from flask_jwt_extended import (
     get_jwt_identity
 )
 from instance.config import app_config
-from .api.v1 import views as v1_views
-from .api.v2 import views as v2_views
+from .api.v1.views import meetup_view,questions_view,user_view
+from .api.v2.views.meetup_view import meetup_v2
+from .api.v2.views.user_view import user_v2
+from .api.v2.views.questions_view import v2 as question_v2
 def create_app(config):
     '''function creating the flask app'''
     app = Flask(__name__)
 
     # register blueprints
-    app.register_blueprint(v1_views.meetup_view)
-    app.register_blueprint(v2_views.meetup_view)
-    app.register_blueprint(v1_views.questions_view.v1)
-    app.register_blueprint(v2_views.questions_view.v2)
-    app.register_blueprint(v1_views.user_view)
-    app.register_blueprint(v2_views.user_view)
+    app.register_blueprint(meetup_view.meetup)
+    app.register_blueprint(questions_view.v1)
+    app.register_blueprint(user_view.user)
+    app.register_blueprint(question_v2)
+    app.register_blueprint(meetup_v2)
+    app.register_blueprint(user_v2)
     app.config.from_object(app_config[config])
 
     # app.config.from_object(confi)
     app.config['JWT_SECRET_KEY'] = "@2ekj@#02ks-"
-    # jwt= JWTManager(app)
+    jwt= JWTManager(app)
     
     return app
