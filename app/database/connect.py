252413ffab83121e421c170c8e2259fd26234d3e
bencyn <bensonnjung39@gmail.com>
2019-01-17 10:18:03 +0300

    [ch #63223314] fix dns errors

--- app/database/connect.py
+++ app/database/connect.py
@@ -5,7 +5,8 @@ from app.database import migrations
 def connect_db(url):
     """ initiate a database connection"""
 
-    url = current_app.config.get('DATABASE_URL')
+    # url = current_app.config.get('DATABASE_URL')
+    # current_app.config.get()
     try:
         conn = psycopg2.connect(url)
         return conn
@@ -14,16 +15,17 @@ def connect_db(url):
 
 def db_init():
     """ setup database connection """
-    url = current_app.config['DATABASE_URL']
+    url = current_app.config.get['DATABASE_URL']
     conn = connect_db(url)
     return conn
 
 def test_db_init():
     """" setup database in test environment """
 
-    url = current_app.config['TEST_DATABASE_URL']
+    url = os.getenv('TEST_DATABASE_URL')
     conn = connect_db(url)
     create_tables(conn)
+    return conn
 
 def create_tables(conn):
     curr = conn.cursor()
@@ -32,3 +34,16 @@ def create_tables(conn):
     for query in tables:
         curr.execute(query)
     conn.commit()
+    
+
+def drop_tables():
+    test_url = os.getenv('TEST_DATABASE_URL')
+    conn = connect_db(test_url)
+    curr = conn.cursor()
+    queries = migrations.tables_to_drop()
+    try:
+        for query in queries:
+            curr.execute(query)
+        conn.commit()
+    except:
+        print("Fail")
