cc6d00ab9b782ca4c0d76468244ad5ce80261156
bencyn <bensonnjung39@gmail.com>
2019-01-18 08:43:59 +0300

    [chore #163300021] add header information to relevant  requests

--- app/tests/v1/test_users.py
+++ app/tests/v1/test_users.py
@@ -5,36 +5,6 @@ from app import create_app
 app = create_app("testing")
 
 class TestUser(BaseTest):
-    def setUp(self):
-        """ defining test data"""
-        app.config.from_object(instance.config.TestingConfig)
-        self.client = app.test_client()
-
-        self.users =[
-            {
-                "firstname" :"benson",
-                "lastname": "kamaa",
-                "othername" :"wamolito",
-                "email" :"bensonnjung39@gmail.com",
-                "password":"ben742285",
-                "phoneNUmber":"0790561841",
-                "username" :"bencyn",
-                "isAdmin" :"0",
-            },
-            {
-                "firstname" :"benson",
-                "lastname": "kamaa",
-                "othername" :"wamolito",
-                "email" :"bensonnjung39@gmail.com",
-                "password":"ben74285",
-                "phoneNUmber":"0790561841",
-                "username" :"njeru",
-                "isAdmin" :"0",
-            }
-        ]
-        self.register_url = 'api/v1/users/'
-        self.get_url = 'api/v1/users/all'
-        self.login_url = 'api/v1/users/login'
 
     def test_if_user_can_create_account(self):
         with self.client:
@@ -66,24 +36,29 @@ class TestUser(BaseTest):
         self.assertEqual(response.status_code,400)
     
     def test_get_all_users(self):
-        response = self.client.get(self.get_url,content_type = "application/json")
+     
+        response = self.client.get(self.get_url,
+                                    content_type = "application/json",
+                                    headers=self._get_header()
+                                )
         self.assertEqual(response.status_code, 200)
         
 
     def test_get_user_by_id(self):
         self.get_by_id_url = 'api/v1/users/2'
-        self.client.post(self.register_url, data = json.dumps(self.users[0]), content_type="application/json")
-        self.client.post(self.register_url, data = json.dumps(self.users[1]), content_type="application/json")
+        
+        response = self.client.get(self.get_by_id_url, 
+                                    content_type="application/json",
+                                    headers=self._get_header())
 
-        response = self.client.get(self.get_by_id_url, content_type="application/json")
         self.assertEqual(response.status_code,200)
         result = json.loads(response.data.decode('utf-8'))
         self.assertEqual(result["username"],self.users[1]["username"])
 
+  
     def test_if_user_can_login(self):
         """ tests if user can login successfully with correct credentials """
-
-        self.loging_data = {"username":"bencyn","password":"ben742285"}
+       
         self._post_register_request(self.users[0])
         response = self._post_login_request(self.loging_data)
         self.assertEqual(response.status_code,201)
@@ -97,6 +72,7 @@ class TestUser(BaseTest):
         """tests if user can login with an empty username string provided"""
 
         self.username_empty={"username":"","password":"ben742285"}
+
         self._post_register_request(self.users[0])
         response = self._post_login_request(self.username_empty)
 
@@ -124,26 +100,5 @@ class TestUser(BaseTest):
         result= json.loads(response.data)
         self.assertEqual(result["error"],"Bad request: attach missing fields")
     
-    def _post_login_request(self,input=""):
-        """ sends a post login request with the input passed as data """
-        
-        if input:
-            self.data = json.dumps(input)
-            response = self.client.post(self.login_url, data = self.data, content_type="application/json")
-        else:
-            response = self.client.post(self.login_url,content_type="application/json")
 
-        return response
-   
-    def _post_register_request(self,input=""):
-        """ send a post regiter request with the input passed as data"""
-        
-        if input is True:
-            response = self.client.post(self.register_url, data = self.data, content_type="application/json")
-        else:
-            response = self.client.post(self.register_url,content_type="application/json")
-
-        return response
-       
-        
     
