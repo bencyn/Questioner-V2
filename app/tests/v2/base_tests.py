286ddb5eefe3923d022a8142911a6f82fbaa7117
bencyn <bensonnjung39@gmail.com>
2019-01-21 02:42:34 +0300

    [chore #163300021] refactor code

--- app/tests/v2/base_tests.py
+++ app/tests/v2/base_tests.py
@@ -1,9 +1,7 @@
 import json ,unittest,instance,datetime
 from app import create_app
 from ... import create_app
-# from ...api.v2.database.database_connection import (create_database_tables,
-#                                                     drop_all_tables, 
-#                                                     all_test_data)
+
 from app.database.connect import Connection
 app = create_app("testing")
 class BaseTest(unittest.TestCase):
@@ -13,7 +11,7 @@ class BaseTest(unittest.TestCase):
         self.app = create_app("testing")
         self.client = self.app.test_client()
         with self.app.app_context():
-            self.test_db = Connection().test_db_init
+            self.test_db = Connection().test_db_init()
         """ defining test data"""
         
         # users test data
