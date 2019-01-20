37ba9fba3a66e58884a9d473a050193d521f5bf4
bencyn <bensonnjung39@gmail.com>
2019-01-21 01:41:43 +0300

    [chore #163300021] define user register and login functions

--- app/api/v2/models/user_model.py
+++ app/api/v2/models/user_model.py
@@ -1,12 +1,17 @@
 from datetime import datetime
+import psycopg2, os
+from app.database.connect import Connection
+from flask import Flask, json, jsonify
 from .base_model import BaseModel
-from app.database.connect import db_init
+
+
 class User(BaseModel):
     """ user class """
 
     def __init__(self):
         """initialize and define objects """
-        pass
+        BaseModel.__init__(self)
+        self.conn=None
 
     def create_user(self,**kwargs):
         """ create a user account in users table """
@@ -15,51 +20,42 @@ class User(BaseModel):
         self.lastname= kwargs['lastname']
         self.othername= kwargs['othername']
         self.email= kwargs['email']
-        self.phoneNumber= kwargs['phoneNumber']
+        self.phone_number= kwargs['phone_number']
         self.username= kwargs['username']
         self.password= kwargs['password']
-        self.registered= datetime.now()
-        self.isAdmin=kwargs.get('isAdmin','0')
-        user ={"firstname":self.firstname,"lastname":self.lastname,"othername":self.othername,"email":self.email,
-                "phoneNumber":self.phoneNumber,"username":self.username,"password":self.password,"isAdmin":self.isAdmin,}
+        self.registered= str(datetime.now())
+        self.is_admin=kwargs.get('is_admin','0')
 
-        # check if user exists
-        if self.check_if_exists(table="users", field="username", data=self.username):
-            return False
-        
-        curr = db_init().cursor()
-        query = """ INSERT into users (firstname,lastname,othername,phoneNumber,username,
-            password,registered,isAdmin) values('{}', '{}', '{}', '{}','{}', '{}', '{}', '{}') RETURNING users.id 
-            """.format(self.firstname, self.lastname, self.othername,self.phoneNumber,self.username, 
-                    self.password,self.registered,self.isAdmin)
+        sql = """ INSERT INTO users (firstname,lastname,othername,email,phone_number,username,password,is_admin)
+                VALUES('{}','{}','{}','{}','{}','{}','{}','{}') RETURNING users.username;""".format(self.firstname,self.lastname,self.othername,self.email,self.phone_number,self.username,
+                                    self.password,self.is_admin)
 
-        # self.cursor.execute(query)
-        curr.execute(query)
-        # self.conn.commit(),
-        db_init().commit()
-        # self.user = self.cursor.fetchone()[0]
-        self.user =curr.fetchone()[0]
-        # self.cursor.close()
-        curr.close()
-        return self.user
+        check_username = self.check_if_exists("users","username",self.username)
+        check_email = self.check_if_exists("users","email",self.email)
 
-    def get_user_by_username(self,username):
-        """ get username by username """
-        self.username =username
-        curr = db_init().cursor()
-        query="""SELECT username, password FROM users WHERE username = '{}'""".format(self.username)
-        curr.execute(query)
-        self.user = curr.fetchone()
-        curr.close()
+        if check_username:
+            return jsonify({
+                "status": 400,
+                "error": "{} already exists".format("username")
+            }), 400
+        elif check_email:
+            return jsonify({
+                "status": 400,
+                "error": "{} already exists".format("email")
+            }), 400
+        else:
+            save_user=self.save_data(sql,**kwargs)
+            return save_user
+   
+    def login_user(self):
+        """"""
+        pass
+    # def get_user_by_id(self,id):
+    #     self.id =id
+    #     cur=conn.cursor()
+    #     query="""SELECT * FROM users WHERE id ={}""".format(self.id)
+    #     cur.execute(query)
+    #     self.user=cur.fetchall()
+    #     # curr.close()
     
-        return self.user
-
-    def get_user_by_id(self,id):
-        self.id =id
-        curr = db_init().cursor()
-        query="""SELECT * FROM users WHERE id = '{}'""".format(self.id)
-        curr.execute(query)
-        self.user = curr.fetchone()
-        curr.close()
-    
-        return self.user
+    #     return self.user
