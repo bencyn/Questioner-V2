20b0588f4abc2f7e86e9d133a01408fc329cffbe
bencyn <bensonnjung39@gmail.com>
2019-01-22 23:25:54 +0300

    [feature #163351740] create save user and get users  queries

--- app/api/v2/models/user_model.py
+++ app/api/v2/models/user_model.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-import psycopg2, os
-from app.database.connect import db_init
+# import psycopg2, os
+# from app.database.connect import db_init
 from flask import Flask, json, jsonify
 from .base_model import BaseModel
 
@@ -10,8 +10,8 @@ class User(BaseModel):
 
     def __init__(self):
         """initialize and define objects """
-        pass
-
+        BaseModel().__init__()
+        
     def create_user(self,**kwargs):
         """ create a user account in users table """
         
@@ -26,7 +26,7 @@ class User(BaseModel):
         self.is_admin=kwargs.get('is_admin','0')
 
         sql = """ INSERT INTO users (firstname,lastname,othername,email,phone_number,username,password,is_admin)
-                VALUES('{}','{}','{}','{}','{}','{}','{}','{}') RETURNING users.username;""".format(self.firstname,self.lastname,self.othername,self.email,self.phone_number,self.username,
+                VALUES('{}','{}','{}','{}','{}','{}','{}','{}') RETURNING users.id;""".format(self.firstname,self.lastname,self.othername,self.email,self.phone_number,self.username,
                                     self.password,self.is_admin)
 
         check_username = self.check_if_exists("users","username",self.username)
@@ -43,8 +43,16 @@ class User(BaseModel):
                 "error": "{} already exists".format("email")
             }), 400
         else:
-            save_user=self.save_data(sql,**kwargs)
-            return save_user
+            save_user=self.save_data(sql)
+            # fetch user
+            user = self.get_by_key("users","id",save_user["id"])
+
+            return jsonify({"status": 201,
+                    "user":user,
+                    "message":"user registered successfully",
+                    "token":kwargs['token']
+                }), 201
+
    
     def login_user(self):
         """"""
